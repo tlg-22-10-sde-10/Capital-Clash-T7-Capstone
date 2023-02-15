5e69475ba31bc75ec1e9b771240ae27829e1d077
Kiaya Anderson <kiaya.anderson@tlgcohort.com>
2023-02-15 09:26:24 -0800

    Working on adding background image to GUI; updated file structure

---
+++ src/main/java/game/GuiConstructor.java
@@ -0,0 +1,14 @@
+package game;
+
+import javax.swing.*;
+
+public class GuiConstructor extends JFrame {
+   public GuiConstructor(){
+       setTitle("Capital Clash 2.0");
+       setSize(500,500);
+       setLocationRelativeTo(null);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setVisible(true);
+   }
+
+}
