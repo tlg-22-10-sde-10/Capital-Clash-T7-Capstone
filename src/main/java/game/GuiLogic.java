4a120136c2f901d92f986c18c1b274ee705d6b5b
Melendez <henry.melendez@tlgcohort.com>
2023-02-15 08:18:07 -0800

    added gui logic file

---
+++ src/main/java/game/GuiLogic.java
@@ -0,0 +1,16 @@
+package game;
+
+import javax.swing.*;
+
+public class GuiLogic {
+
+    private static final JFrame frame = new GuiClient();
+    JButton newGame;
+
+
+
+    public void initGui(){
+
+    }
+
+}
