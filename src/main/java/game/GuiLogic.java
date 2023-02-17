8ba1fbb479b00bfeca34dc99c126cf96c546261c
ShaBlai <Shawn.Blair@tlgcohort.com>
2023-02-17 13:22:10 -0800

    Started attaching buy logic to trading room GUI, AB#1215

--- src/main/java/game/GuiLogic.java
+++ src/main/java/game/GuiLogic.java
@@ -2,11 +2,14 @@ package game;
 
 import guigamelogic.CountdownTimer;
 import guigamelogic.GameStory;
+import guigamelogic.TradingRoom;
+import ui.GlobalMethodsAndAttributes;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 
 
 public class GuiLogic extends javax.swing.JFrame {
@@ -19,6 +22,8 @@ public class GuiLogic extends javax.swing.JFrame {
     private JButton continueButton;
     private JButton endTradingDay;
     private JButton buyStock;
+    private JButton submitBuyStockMenuButton;
+    private JButton cancelBuyStockMenuButton;
     private JButton sellStock;
     private JButton sleep;
     private JButton playComputer;
@@ -33,23 +38,29 @@ public class GuiLogic extends javax.swing.JFrame {
     private JLabel timeRemaining;
 
 
-
-
     //Game Panels
     private JPanel welcomeBanner;
     private JPanel tradingTimerPanel;
     private JPanel newsTicker;
-
-
+    private JPanel buyMenuStocksPanel;
+    private JPanel buyMenuStocksButtonPanel;
 
     //Game Text & Panes
     private JTextArea gameStoryText;
     private JScrollPane scrollPane;
+    private JTextArea buyMenuStocksListing;
 
     // Game ImageIcons
     private ImageIcon tradingRoomBackground;
     private ImageIcon bedroomBackground;
     private ImageIcon img;
+
+    //Game JFrames
+    private JFrame buyMenuPopup;
+
+
+    //Game Day Counter
+    public static int dayCounter = 1;
 
 
     public GuiLogic() {
@@ -59,10 +70,18 @@ public class GuiLogic extends javax.swing.JFrame {
 
     //Initializing the GUI, new game and quit buttons
     public void initGui() {
+
+        //Unifies the look between different MAC/Windows platforms
         try {
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        //Initializes the Global Variables from the console game
+        try {
+            GlobalMethodsAndAttributes.initializeGlobalInstances();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
         gameTitle_CAPITAL = new JLabel("CAPITAL");
@@ -128,7 +147,7 @@ public class GuiLogic extends javax.swing.JFrame {
 
         gameStoryText.setBounds(75, 60, 350, 350);
         gameStoryText.setFont(new Font("Playfair Display", Font.BOLD, 12));
-        gameStoryText.setBackground(new Color(0,0,0,65));
+        gameStoryText.setBackground(new Color(0, 0, 0, 65));
         gameStoryText.setForeground(Color.black);
         gameStoryText.setEditable(false);
         gameStoryText.setLineWrap(true);
@@ -152,87 +171,106 @@ public class GuiLogic extends javax.swing.JFrame {
                 guiTradingRoom();
             }
         });
+   }
+
+    public void guiTradingRoom() {
+        continueButton.setVisible(false);
+        //setting the variables and placements
+        welcomeBanner = new JPanel();
+        tradingTimerPanel = new JPanel();
+        endTradingDay = new JButton();
+        breakingNews = new JLabel();
+        newsTicker = new JPanel();
+        buyStock = new JButton();
+        sellStock = new JButton();
+        timeRemaining = new JLabel(CountdownTimer.getTimeRemaining());
+
+
+        //start trading for the day
+        CountdownTimer.startTimer(5);
+
+        //changes the background image
+        tradingRoomBackground = new ImageIcon("src/main/resources/Trading-Room.jpg");
+        backgroundImg = new JLabel(tradingRoomBackground);
+
+        //setting the background picture and location
+        backgroundImg.setBounds(0, 0, 800, 600);
+
+        //setting the welcome banner location and color
+        welcomeBanner.setBounds(0, 0, 800, 25);
+        welcomeBanner.setBackground(new Color(0, 0, 0, 125));
+
+        //setting the timer descriptions
+
+        //setting the trading timer location and color
+        tradingTimerPanel.setBounds(40, 40, 100, 20);
+        tradingTimerPanel.setBackground(new Color(0, 0, 0, 125));
+        tradingTimerPanel.add(timeRemaining);
+
+        //setting the location end trading day/go to room button
+        endTradingDay.setBounds(650, 40, 105, 40);
+        endTradingDay.setBackground(Color.GREEN);
+        endTradingDay.setText("End Trading Day");
+
+        //setting the location of the breaking news header
+        breakingNews.setBounds(330, 100, 300, 50);
+        breakingNews.setText("*** BREAKING NEWS ***");
+
+        //setting the location of the news ticker
+        newsTicker.setBounds(100, 140, 600, 40);
+        newsTicker.setBackground(new Color(0, 0, 0, 125));
+
+        //setting the location and description of the buystock button
+        buyStock.setBounds(170, 415, 105, 40);
+        buyStock.setText("Buy Stock");
+        buyStock.setBackground(Color.GREEN);
+
+        //setting the location and description of the sell button
+        sellStock.setBounds(510, 415, 105, 40);
+        sellStock.setText("Sell Stock");
+        sellStock.setBackground(Color.ORANGE);
+
+
+        //adding the elements to the frame
+        frame.setContentPane(backgroundImg);
+        frame.getContentPane().add(welcomeBanner);
+        frame.getContentPane().add(tradingTimerPanel);
+        frame.getContentPane().add(endTradingDay);
+        frame.getContentPane().add(breakingNews);
+        frame.getContentPane().add(newsTicker);
+        frame.getContentPane().add(buyStock);
+        frame.getContentPane().add(sellStock);
+
+
+        buyStock.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buyMenuCreator();
+
+            }
+        });
+
+
+//        sellStock.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//
+//
+//            }
+//        });
+
+
+        endTradingDay.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.repaint();
+                bedRoom();
+            }
+        });
     }
-        public void guiTradingRoom(){
-            continueButton.setVisible(false);
-            //setting the variables and placements
-            welcomeBanner = new JPanel();
-            tradingTimerPanel = new JPanel();
-            endTradingDay = new JButton();
-            breakingNews = new JLabel();
-            newsTicker = new JPanel();
-            buyStock = new JButton();
-            sellStock = new JButton();
-            timeRemaining = new JLabel(CountdownTimer.getTimeRemaining());
 
-
-            //start trading for the day
-            CountdownTimer.startTimer(5);
-
-            //changes the background image
-            tradingRoomBackground = new ImageIcon("src/main/resources/Trading-Room.jpg");
-            backgroundImg = new JLabel(tradingRoomBackground);
-
-            //setting the background picture and location
-            backgroundImg.setBounds(0, 0, 800,600);
-
-            //setting the welcome banner location and color
-            welcomeBanner.setBounds(0,0, 800, 25);
-            welcomeBanner.setBackground(new Color(0, 0, 0, 125));
-
-            //setting the timer descriptions
-
-            //setting the trading timer location and color
-            tradingTimerPanel.setBounds(40,40,100,20);
-            tradingTimerPanel.setBackground(new Color(0,0,0,125));
-            tradingTimerPanel.add(timeRemaining);
-
-            //setting the location end trading day/go to room button
-            endTradingDay.setBounds(650, 40,105,40);
-            endTradingDay.setBackground(Color.GREEN);
-            endTradingDay.setText("End Trading Day");
-
-            //setting the location of the breaking news header
-            breakingNews.setBounds(330,100,300,50);
-            breakingNews.setText("*** BREAKING NEWS ***");
-
-            //setting the location of the news ticker
-            newsTicker.setBounds(100, 140, 600,40);
-            newsTicker.setBackground(new Color(0, 0, 0, 125));
-
-            //setting the location and description of the buystock button
-            buyStock.setBounds(170, 415, 105,40);
-            buyStock.setText("Buy Stock");
-            buyStock.setBackground(Color.GREEN);
-
-            //setting the location and description of the sell button
-            sellStock.setBounds(510, 415, 105,40);
-            sellStock.setText("Sell Stock");
-            sellStock.setBackground(Color.ORANGE);
-
-
-            //adding the elements to the frame
-            frame.setContentPane(backgroundImg);
-            frame.getContentPane().add(welcomeBanner);
-            frame.getContentPane().add(tradingTimerPanel);
-            frame.getContentPane().add(endTradingDay);
-            frame.getContentPane().add(breakingNews);
-            frame.getContentPane().add(newsTicker);
-            frame.getContentPane().add(buyStock);
-            frame.getContentPane().add(sellStock);
-
-
-            endTradingDay.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    frame.getContentPane().removeAll();
-                    frame.repaint();
-                    bedRoom();
-                }
-            });
-        }
-
-        public void bedRoom() {
+    public void bedRoom() {
         //setting the bedroom background
         bedroomBackground = new ImageIcon("src/main/resources/bedroom.jpg");
         roomBackgroundImg = new JLabel(bedroomBackground);
@@ -242,21 +280,21 @@ public class GuiLogic extends javax.swing.JFrame {
 
 
         //setting the descriptions for the sleep button
-        sleep.setBounds(570,350,160,50);
+        sleep.setBounds(570, 350, 160, 50);
         sleep.setText("Sleep/Next Day?");
-        sleep.setBackground(new Color(0,0,85,125));
+        sleep.setBackground(new Color(0, 0, 85, 125));
         sleep.setForeground(Color.CYAN);
 
 
         //setting the descriptions for the playcomputer button
         playComputer.setBounds(15, 240, 160, 50);
         playComputer.setText("Play Computer Game?");
-        playComputer.setBackground(new Color(0,0,85,125));
+        playComputer.setBackground(new Color(0, 0, 85, 125));
         playComputer.setForeground(Color.CYAN);
 
 
         //setting the bounds of the image background
-        roomBackgroundImg.setBounds(0,0,800,600);
+        roomBackgroundImg.setBounds(0, 0, 800, 600);
 
 
         //adding to the frame
@@ -264,7 +302,7 @@ public class GuiLogic extends javax.swing.JFrame {
         frame.getContentPane().add(sleep);
         frame.getContentPane().add(playComputer);
 
-            //commented out for future use
+        //commented out for future use
 //        playComputer.addActionListener(new ActionListener() {
 //
 //            @Override
@@ -275,15 +313,70 @@ public class GuiLogic extends javax.swing.JFrame {
 //        });
 
 
-         sleep.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    frame.getContentPane().removeAll();
-                    frame.repaint();
-                    guiTradingRoom();
-                }
-            });
-        }
+        sleep.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().removeAll();
+                frame.repaint();
+                guiTradingRoom();
+            }
+        });
+    }
+
+
+    public void buyMenuCreator() {
+        //
+        buyMenuStocksListing = new JTextArea();
+        buyMenuStocksPanel = new JPanel();
+        buyMenuPopup = new JFrame();
+        submitBuyStockMenuButton = new JButton("Submit");
+        cancelBuyStockMenuButton = new JButton("Cancel");
+        buyMenuStocksButtonPanel = new JPanel();
+
+
+        //Setting the frame to popup
+        buyMenuPopup.setVisible(true);
+        buyMenuPopup.setTitle("Buy Stock");
+        buyMenuPopup.setSize(600, 400);
+        buyMenuPopup.setLocationRelativeTo(null);
+        buyMenuPopup.setResizable(false);
+        buyMenuPopup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        //setting buymenu stock listing location
+        buyMenuStocksPanel.setSize(600,200);
+        buyMenuStocksListing.setEditable(false);
+        buyMenuStocksPanel.add(TradingRoom.showStockInventory(buyMenuStocksListing));
+
+        //setting the submit button location
+        submitBuyStockMenuButton.setBounds(300,300,10,10);
+
+        //setting the cancel button location
+        //cancelBuyStockMenuButton.setBounds(300,300,25,25);
+
+//        //setting buymenu button holder panel
+//        buyMenuStocksButtonPanel.setBounds(500,500,500,500);
+//        buyMenuStocksButtonPanel.setSize(100,100);
+//        buyMenuStocksButtonPanel.add(submitBuyStockMenuButton);
+
+
+        buyMenuPopup.getContentPane().add(buyMenuStocksPanel);
+        buyMenuPopup.getContentPane().add(submitBuyStockMenuButton);
+
+        //buyMenuPopup.getContentPane().add(cancelBuyStockMenuButton);
+
 
     }
+
+}
+
+
+
+
+
+
+
+
+
+
 
--- src/main/java/game/Images.java
+++ src/main/java/game/Images.java
@@ -1,24 +1,34 @@
 package game;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Images extends JPanel {
 
     private final Image image;
 
 
-    public Images(String img){
-        this.image = new ImageIcon(img).getImage();
-        Dimension size = new Dimension(image.getWidth(null),image.getHeight(null));
-        setSize(size);
-        setLayout(null);
+    public Images(String img) {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(img)) {
+
+            //noinspection DataFlowIssue
+            this.image = ImageIO.read(input);
+            Dimension size = new Dimension(image.getWidth(null), image.getHeight(null));
+            setSize(size);
+            setLayout(null);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
-    public void paintComponent(Graphics g){
-        g.drawImage(image,0,0,this.getWidth(),this.getHeight(),null);
+    public void paintComponent(Graphics g) {
+        g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
 
     }
 }
