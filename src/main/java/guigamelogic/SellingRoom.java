cb4ccb482167fb27e0dafc58baff899be2c60449
Melendez <henry.melendez@tlgcohort.com>
2023-02-21 09:21:31 -0800

    AB#1251 fix stock sell method bugs

--- src/main/java/guigamelogic/SellingRoom.java
+++ src/main/java/guigamelogic/SellingRoom.java
@@ -1,5 +1,6 @@
 package guigamelogic;
 
+import stock.Stock;
 import ui.GlobalMethodsAndAttributes;
 
 import javax.swing.*;
@@ -10,55 +11,52 @@ import static ui.GlobalMethodsAndAttributes.*;
 
 public class SellingRoom {
 
-    public static void menuTwoSell(String stockSymbol, String quantityInput) throws FileNotFoundException {
+    public static void menuTwoSell(int day, String stockSymbol, int numberOfStockSold, JTextArea text) throws FileNotFoundException {
+        if(!isValidStockSymbol(stockSymbol)){
 
-        GlobalMethodsAndAttributes.initializeGlobalInstances();
+        }
 
-
-        if (playerStockMap.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "***No Current Holdings. Transaction cannot be completed***"
-                    , "Error", JOptionPane.ERROR_MESSAGE);
-        } else {
-                ArrayList<String> playerStockList = new ArrayList<>(playerStockMap.keySet());
-                showHoldings(playerStockList);
-
-                if (!playerStockMap.containsKey(stockSymbol)) {
-                    JOptionPane.showMessageDialog(null, "**This Stock is not in your holdings***\nPlease try again.***"
-                            , "Error", JOptionPane.ERROR_MESSAGE);
-                    showHoldings(playerStockList);
-                    return;
-                }
-
-                if (!isPositiveInteger(quantityInput)) {
-                    JOptionPane.showMessageDialog(null, "***Your input is not an integer. Fractional Shares not allowed.\nPlease try again.***"
-                            , "Error", JOptionPane.ERROR_MESSAGE);
-                    return;
-                }
-
-                int quantity = Integer.parseInt(quantityInput);
-                int currentQuantity = playerStockMap.get(stockSymbol);
-                if ( currentQuantity >= quantity) {
-                    double pricePerShare = inventory.findBySymbol(stockSymbol).getCurrentPrice();
-                    double totalSalesPrice = quantity * pricePerShare;
-                    player.getAccount().calculateBalance(totalSalesPrice);
-
-                    int newQuantity  = currentQuantity - quantity;
-                    if (newQuantity == 0) {
-                        playerStockMap.remove(stockSymbol);
-                    }
-                    else {
-                        playerStockMap.put(stockSymbol,newQuantity);
-                    }
-
-                    JOptionPane.showMessageDialog(null, "***Successfully Sold " + quantityInput + " shares of " +
-                            inventory.findBySymbol(stockSymbol).getStockName() + "***", "Success", JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(null, "***Please Try again and enter a valid stock quantity.***", "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                }
+        Stock playerStock = inventory.findBySymbol(stockSymbol);
+        if(!hasSufficientStock(playerStock,numberOfStockSold)){
+            showNotEnoughStockMessage(text);
+        }
+        else {
+            sellStock(day, playerStock, numberOfStockSold, text);
         }
 
     }
+
+    private static void sellStock(int day, Stock playerStock, int numberOfStockSold, JTextArea text){
+        playerStockMap.put(playerStock.getSymbol(), playerStockMap.get(playerStock.getSymbol())- numberOfStockSold);
+
+        if(playerStockMap.get(playerStock.getSymbol()) == 0){
+            playerStocks.remove(playerStock.getSymbol());
+        }
+
+        player.setStockNames(playerStocks);
+        player.setStocks(playerStockMap);
+        player.getAccount().calculateBalance(numberOfStockSold * playerStock.getCurrentPrice());
+        showSuccessfulSaleMessage(numberOfStockSold,playerStock,text);
+    }
+
+    private static boolean hasSufficientStock(Stock playerStock, int numberOfStockSold){
+        return playerStockMap.containsKey(playerStock.getSymbol()) && playerStockMap.get(playerStock.getSymbol()) >= numberOfStockSold;
+    }
+
+
+    private static void showSuccessfulSaleMessage(int numberOfStockSold, Stock playerStock, JTextArea text){
+        text.append("***Successfully Sold " + numberOfStockSold +
+                " share of " + playerStock.getStockName() + " ***\n");
+    }
+
+    private static void showNotEnoughStockMessage(JTextArea jTextArea){
+        jTextArea.append("***UnAuthiroized sale! Not Enough Stock!***\n");
+    }
+
+    private static boolean isValidStockSymbol(String stockSymbol){
+        return inventory.findBySymbol(stockSymbol) != null;
+    }
+
 
     public static JTextArea showPlayerHoldings(JTextArea sellMenuTextArea) {
 
